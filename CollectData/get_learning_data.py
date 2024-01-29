c5cd2b3c31c583ac0fcfe6d97b12fd0741c2a93b
bro-tec <ig0brudi@gmail.com>
2024-01-29 22:24:45 +0100

    continued working on lstm and optimized generator

--- CollectData/get_learning_data.py
+++ CollectData/get_learning_data.py
@@ -9,8 +9,7 @@ icons = ["clear-day", "clear-night", "partly-cloudy-day", "partly-cloudy-night",
 
 
 def load_stations_csv():
-    df = pd.read_csv('stations.csv',dtype={'ID': object}) # otherwise lstm cant load this
-    return df
+    return pd.read_csv('stations.csv',dtype={'ID': object}) # otherwise lstm cant load this
 
 def euclidean(chosen, unchosen):
     cols=['lon','lat','height']
@@ -19,7 +18,7 @@ def euclidean(chosen, unchosen):
 
 def chooseByNearest(chosen, unchosen):
     dists = euclidean(chosen.head(1), unchosen)
-    dists = dists.sort_values(by=['dist'], ascending=True)
+    dists.sort_values(by=['dist'], ascending=True, inplace=True)
     return dists
 
 def getCities(cityP,cityName):
@@ -32,22 +31,22 @@ def getCities(cityP,cityName):
     return chooseByNearest(chosen, unchosen)
 
 def filter_data(data):
-    data = data.drop(["source_id"], axis=1)
-    data["icon"] = [icons.index(_) for _ in data["icon"]]
-    data["condition"] = [icons.index(_) for _ in data["condition"]]
+    data.drop(["source_id"], axis=1, inplace=True)
+    # data["icon"] = [icons.index(_) for _ in data["icon"]]
+    # data["condition"] = [icons.index(_) for _ in data["condition"]]
+    data["icon"] = data["icon"].isin(icons).astype(float)
+    data["condition"] = data["condition"].isin(icons).astype(float)
     if "fallback_source_ids" in data:
-        data = data.drop(["fallback_source_ids"], axis=1)
-    data = data.fillna(0)
+        data.drop(["fallback_source_ids"], axis=1, inplace=True)
+    data.fillna(0,inplace=True)
     return data
 
 
 def joinData(cityID, cities, lc, date):
-    # print(cityID, cities, date)
     data = dwd.getWeatherByStationIDDate(cityID, date)
     
     if data[0]== "error":
         return pd.DataFrame(columns=["error"])
-    # print(json.dumps(data, indent = 4, sort_keys = True))
     data =pd.DataFrame(data)
     data = filter_data(data)
     i = 0
@@ -61,11 +60,9 @@ def joinData(cityID, cities, lc, date):
         newdata = filter_data(newdata)
         data = pd.merge(data, newdata, on="timestamp", how="outer", suffixes=("", "_" + str(i+4-lc)))
 
-        #data = data.join(newdata, lsuffix='', rsuffix='{i}', how='outer').reset_index()
-    # print("joinData",data.columns)
-    # data = data.drop(["source_id", "source_id_0", "source_id_1", "source_id_2", "source_id_3", "fallback_source_ids_3", "fallback_source_ids_2", "fallback_source_ids_1", "fallback_source_ids_0"], axis=1)
     data = data.drop(["timestamp"], axis=1)
     return data
+
 
 def label(cityID, date):
     data = dwd.getWeatherByStationIDDate(cityID, date)
@@ -75,7 +72,7 @@ def label(cityID, date):
     label_Data = filter_data(label_Data)
     return label_Data
 
-def gen_trainDataHourly():
+def gen_trainDataDaily():
     cityP = load_stations_csv()
     minTime = dt.strptime(min(cityP["start"]), '%Y-%m-%d %H:%M:%S')
     actualTime = dt.now()
@@ -96,8 +93,30 @@ def gen_trainDataHourly():
             label_Data = label_Data.drop(["timestamp"], axis=1)
             if "error" in label_Data:
                 continue
+            yield train_Data.values.to(), label_Data.values.tolist()
 
-            yield train_Data.values.tolist(), label_Data.values.tolist()
+def gen_trainDataHourly():
+    cityP = load_stations_csv()
+    minTime = dt.strptime(min(cityP["start"]), '%Y-%m-%d %H:%M:%S')
+    actualTime = dt.now()
+    duration = actualTime - minTime
+    print(duration, "=", actualTime, "-", minTime)
+    for d in range(duration.days-1):
+        date = minTime + td(days=d)
+        for row in cityP.itertuples(index=False):
+            cityName = row.Name
+            cityID = row.ID
+            cities = getCities(cityP,cityName)
+            train_Data = joinData(cityID, cities, 4, date.date())
+            if "error" in train_Data or len(train_Data) > 25:
+                continue
+            date = minTime + td(days=d+1)
+            label_Data = train_Data[["precipitation","pressure_msl","sunshine","temperature","wind_direction","wind_speed","cloud_cover","dew_point","relative_humidity","visibility","wind_gust_direction","wind_gust_speed","condition","precipitation_probability","precipitation_probability_6h","solar","icon"]]
+            # label_Data = label_Data.drop(["timestamp"], axis=1)
+            if "error" in label_Data:
+                continue
+
+            yield train_Data[:-1].to_numpy(),label_Data.iloc[1:].to_numpy()
 
 
 # if __name__ == "__main__":
--- weather_lstm.py
+++ weather_lstm.py
@@ -1,11 +1,13 @@
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import torch
+import os
 # import pandas as pd
 # import tensorflow as tf
 import CollectData.get_learning_data as gld
 from sklearn.model_selection import train_test_split
-from tqdm import tqdm
-from keras.models import Sequential
+# from tqdm import tqdm
+from keras.models import Sequential, load_model
 from keras.layers import Dense
 from keras.layers import LSTM
 # from sklearn.preprocessing import LabelEncoder
@@ -13,18 +15,44 @@ from keras.callbacks import ReduceLROnPlateau, EarlyStopping
 # from keras.preprocessing.sequence import pad_sequences
 # from sklearn.preprocessing import MinMaxScaler
 # from sklearn.metrics import mean_squared_error
+import warnings
+import tensorflow as tf
+
+warnings.filterwarnings('ignore')
 
 # trainData = gld.gen_trainDataHourly()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
 
+# print(tf.compat.v1.keras.backend.tensorflow_backend._get_available_gpus())
 
-model = Sequential()
-model.add(LSTM( input_shape=(85,1), dropout = 0.2, units=52))
-model.add(Dense(17, activation = 'sigmoid' ))
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
+# net = net.to(device) 
+# input = input.to(device) 
+# labels = labels.to(device) 
+
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# print(gpus)
+model = []
+
+def load_own_Model():
+    if os.path.exists("./Models/Hourly.h5"):
+        model = load_model("./Models/Hourly.h5")
+        model.load_weights("./Models/Hourly_weights.h5")
+        history=np.load('./Models/Hourly_history.npy',allow_pickle='TRUE').item()
+        print("Model found")
+        return model, history
+    else:
+        print("Model not found")
+        model = Sequential()
+        model.add(LSTM( input_shape=(85,1), dropout = 0.2, units=52))
+        model.add(Dense(17, activation = 'sigmoid' ))
+        model.compile( optimizer = "adam" , loss = 'binary_crossentropy' , metrics = ['accuracy'] )
+        return model, [] 
 # model.summary()
 # model.compile(loss='mean_squared_error', optimizer='adam')
 # model.fit(X_train, y_train, epochs=1, batch_size=1, verbose=2)
 
-model.compile( optimizer = "adam" , loss = 'binary_crossentropy' , metrics = ['accuracy'] )
 
 early_stopper = EarlyStopping( monitor = 'val_acc' , min_delta = 0.0005, patience = 3 )
 
@@ -36,57 +64,94 @@ callbacks = [ reduce_lr , early_stopper]
 
 
 
-batch_size = 70
+batch_size = 24
 
-X_list = []
-y_list = []
+X_np = np.zeros(shape=(1, 1))
+y_np = np.zeros(shape=(1, 1))
 i=1
 
 for train, label in gld.gen_trainDataHourly():
+
+    if i%25 == 1:
+        X_np = train
+        y_np = label
+    else:
+        X_np += train
+        y_np += label
     print(i)
-    # print(train, label)
-    X_list.extend(train) 
-    y_list.extend(label)
 
-    if i%3 == 0:
-        X_np = np.array(X_list)
-        y_np = np.array(y_list)
-        X_np = X_np.astype(float)
-        y_np = y_np.astype(float)
 
+    if i%25 == 0:
         # Assume X and y are your features and labels
         X_train, X_test, y_train, y_test = train_test_split(X_np, y_np, test_size=0.3, random_state=42)
         # print("X_train",X_train, "X_test",X_test, "y_train",y_train, "y_test",y_test)
         # X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
         # print("X_train",len(X_train), "X_test",len(X_test), "X_val", len(X_val), "y_train",len(y_train), "y_test",len(y_test), "y_val", len(y_val))
-        print("X_train",len(X_train), "X_test",len(X_test), "y_train",len(y_train), "y_test",len(y_test))
+        # print("X_train",len(X_train), "X_test",len(X_test), "y_train",len(y_train), "y_test",len(y_test))
+
+        model, history = load_own_Model()
+
+        train_history = model.fit( X_train, y_train, epochs = 2, batch_size = batch_size, validation_split = 0.1 , verbose = 1 , callbacks = callbacks)
         
+        (len(history))
+        if len(history) > 0:
+            # print("train_history.history", train_history.history)
+            # print("history1", history)
+            for ky in history.keys():
+                extended_values = train_history.history.get(ky, [])
+                history[ky] = history.get(ky, []) + extended_values
+                history[ky] = [0 if v is None else v for v in history[ky]]
+        else:
+            history = dict(train_history.history)
+            # print("history1", history)
+            # print("train_history", train_history.history)
 
-        # encoder = LabelEncoder()
-
-        # encoder.fit(y_train)
-
-        # y_train_transformed = encoder.transform(y_train).reshape(-1,1)
-
-        # y_test_transformed = encoder.transform(y_test).reshape(-1,1)
-
-        # print(y_train_transformed)
-        # print(y_test_transformed)
-
-        # X_train_seq = pad_sequences( X_train)
-
-        # X_test_seq = pad_sequences( X_test)
-
-        # break
-
-        train_history = model.fit( X_train, y_train, epochs = 5, validation_split = 0.1 , verbose = 1 , callbacks = callbacks)
-        #, batch_size = 24
-
-        score = model.evaluate( X_test , y_test , batch_size = batch_size)
+        score = model.evaluate( X_test, y_test, batch_size = batch_size)
 
         print( "Accuracy: {:0.4}".format( score[1] ))
+        np.save('./Models/Hourly_history.npy',history)
+        model.save("./Models/Hourly.h5")
+        model.save_weights("./Models/Hourly_weights.h5")
+# train_history = model.fit( gld.gen_trainDataHourly(), epochs = 1, batch_size = batch_size, validation_data=2, validation_steps=1, verbose = 1 , callbacks = callbacks)
 
-        X_list = []
-        y_list = []
+# history = model.fit_generator(
+#             train_generator,
+#             steps_per_epoch=24,
+#             epochs=5,
+#             validation_data=2,
+#             validation_steps=1,
+#             class_weight=17,
+#             initial_epoch=1,
+#             max_queue_size=15,
+#             workers=8,
+#             callbacks=callbacks
+#             )
+        # score = model.evaluate( gld.gen_trainDataHourly(), batch_size = batch_size)
+
+        X_np = []
+        y_np = []
+
+        print(train_history.history.keys())
+
+        plt.plot(history['accuracy'])
+        plt.plot(history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.yticks([label/2 for label in plt.yticks()[0]])
+        plt.show()
+        # summarize history for loss
+        plt.plot(history['loss'])
+        plt.plot(history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.yticks([label/2 for label in plt.yticks()[0]])
+        plt.show()
+
 
     i+=1
+
+print("Saved model to disk")
